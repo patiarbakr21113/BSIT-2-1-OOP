658f956ebf9c2d2dfd9bc5df4382cb74c6dd315c
patiarbakr21113 <patiarbakr21113@liceo.edu.ph>
2026-09-10 21:42:07 +0800

    Update Admin.java

--- src/Admin.java
+++ src/Admin.java
@@ -1,1 +1,18 @@
+// Admin.java
 
+public class Admin extends User {
+
+    public Admin(int id, String name, String email) {
+        super(id, name, email);
+    }
+
+    @Override
+    public String role() {
+        return "ADMIN";
+    }
+
+    @Override
+    public String permissions() {
+        return "create, read, update, delete";
+    }
+}
